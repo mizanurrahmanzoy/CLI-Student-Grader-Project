73b3ac21ac6aa1d0880ed55953a7fa6990edcdd2
mizanurrahmanzoy <mizan258173@gmail.com>
2026-04-18 01:11:23 +0600

    The Menu Loop

--- main.dart
+++ main.dart
@@ -14,5 +14,27 @@ void main() {
   // Subject List
   final Set<String> subjects = {"Math", "Science", "History", "Literature"};
 
+  // Option
+  Map<int, String> options = {
+    1: "Add Student",
+    2: "Record Score",
+    3: "Add Bonus Points",
+    4: "Add Comment",
+    5: "View All Students",
+    6: "View Report Card",
+    7: "Class Summary",
+    8: "Exit",
+  };
 
+  var i = 0;
+  do {
+    options.forEach((key, value) => print("$key. $value"));
+    print("\nChoose an option:");
+    
+
+
+
+
+    i++;
+  } while (i < 1);
 }
